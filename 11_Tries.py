bf03da5507c275916bb682856f22a24554c9517e
kishoear <icodelikekishorear@gmail.com>
2024-03-09 19:37:58 +0530

    Tries added

--- 11_Tries.py
+++ 11_Tries.py
@@ -1,49 +1,36 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}  # Dictionary to store child nodes
-        self.is_end_of_word = False  # Flag to indicate end of a word
-
+class Node:
+    def __init__(self) -> None:
+        self.children = {}
+        self.isEnd = False
 
 class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-
-    def insert(self, word):
+    def __init__(self) -> None:
+        self.root = Node()
+    
+    def insert(self, word) -> None:
         current_node = self.root
-        for char in word:
-            if char not in current_node.children:
-                current_node.children[char] = TrieNode()
-            current_node = current_node.children[char]
-        current_node.is_end_of_word = True
-
-    def search(self, word):
+        
+        for character in word:
+            if character not in current_node.children:
+                current_node.children[character] = Node()
+            current_node = current_node.children[character]
+        
+        current_node.isEnd = True
+    
+    def search(self, word) -> bool:
         current_node = self.root
-        for char in word:
-            if char not in current_node.children:
+        
+        for character in word:
+            if character not in current_node.children:
                 return False
-            current_node = current_node.children[char]
-        return current_node.is_end_of_word
+            current_node = current_node.children[character]
+        return current_node.isEnd    
+    
+    def remove(self):
+        pass
+            
 
-    def starts_with(self, prefix):
-        current_node = self.root
-        for char in prefix:
-            if char not in current_node.children:
-                return False
-            current_node = current_node.children[char]
-        return True
-
-
-# Example usage
-trie = Trie()
-words = ["apple", "banana", "orange", "app", "applesauce"]
-for word in words:
-    trie.insert(word)
-
-print(trie.search("apple"))  # True
-print(trie.search("banana"))  # True
-print(trie.search("orange"))  # True
-print(trie.search("app"))  # True
-print(trie.search("apples"))  # False
-print(trie.starts_with("app"))  # True
-print(trie.starts_with("or"))  # True
-print(trie.starts_with("pear"))  # False
+if __name__ == "__main__":
+    T = Trie()
+    T.insert("Kishore")
+    print(T.search("Kish"))
